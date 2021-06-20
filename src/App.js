7afeaca8c49d52ba0290d32baac089bf2fd39d76
Oriol Carbo <oriol.carbo@gmail.com>
2021-06-20 22:12:21 +0200

    Add Basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
+
+import API from "./lib/api-service";
+import { CustomModal } from "./components/Modal";
+import { CityCard } from "./components/CityCard";
+import styles from "./App.module.css";
 
 function App() {
+  const [locations, setLocations] = useState([]);
+  const [show, setShow] = useState(false);
+  const [updateList, setUpdateList] = useState(false);
+  const [currentCity, setCurrentCity] = useState(null);
+
+  useEffect(() => {
+    getLocations();
+    return () => {
+      setUpdateList(false);
+    };
+  }, [updateList]);
+
+  const getLocations = async () => {
+    const data = await API.list();
+    setLocations(data);
+  };
+
+  const handleCloseModal = () => {
+    setShow(false);
+    setCurrentCity(null);
+  };
+
+  const handleShowModal = () => setShow(true);
+
+  const deleteCity = async (id) => {
+    await API.remove(id);
+    setUpdateList(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className={styles.container}>
+      <Row
+        style={{ backgroundColor: "orange", paddingBottom: 10, paddingTop: 10 }}
+      >
+        <Col style={{ backgroundColor: "gray" }}>
+          This should be the title centered
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ backgroundColor: "lightblue" }}>
+          <Button onClick={handleShowModal}>Add City</Button>
+        </Col>
+      </Row>
+      <Row
+        className="d-flex justify-content-start w-75 align-self-center"
+        style={{ backgroundColor: "green" }}
+      >
+        {locations.map((location, index) => (
+          <CityCard
+            location={location}
+            index={index}
+            deleteCity={deleteCity}
+            handleShowModal={handleShowModal}
+            setCurrentCity={setCurrentCity}
+          />
+        ))}
+      </Row>
+      <CustomModal
+        show={show}
+        handleClose={handleCloseModal}
+        setUpdateList={setUpdateList}
+        currentCity={currentCity}
+      />
+    </Container>
   );
 }
 
