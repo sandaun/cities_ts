81b7911eb54fa092e11134f5bb676e4c16aa447b
Oriol Carbo <oriol.carbo@gmail.com>
2021-06-21 00:37:32 +0200

    Update App main with naming changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Button, Container, Row, Col, Card } from "react-bootstrap";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 import API from "./lib/api-service";
 import { CustomModal } from "./components/Modal";
+import { CityModal } from "./components/CityModal";
 import { CityCard } from "./components/CityCard";
 import styles from "./App.module.css";
 
 function App() {
   const [locations, setLocations] = useState([]);
-  const [show, setShow] = useState(false);
+  const [showFormModal, setShowFormModal] = useState(false);
+  const [showCity, setShowCity] = useState(false);
   const [updateList, setUpdateList] = useState(false);
   const [currentCity, setCurrentCity] = useState(null);
 
@@ -25,11 +27,14 @@ function App() {
   };
 
   const handleCloseModal = () => {
-    setShow(false);
+    setShowFormModal(false);
     setCurrentCity(null);
   };
 
-  const handleShowModal = () => setShow(true);
+  const handleShowModal = () => setShowFormModal(true);
+
+  const handleShowCityModal = () => setShowCity(true);
+  const handleCloseCityModal = () => setShowCity(false);
 
   const deleteCity = async (id) => {
     await API.remove(id);
@@ -38,36 +43,39 @@ function App() {
 
   return (
     <Container fluid className={styles.container}>
-      <Row
-        style={{ backgroundColor: "orange", paddingBottom: 10, paddingTop: 10 }}
-      >
-        <Col style={{ backgroundColor: "gray" }}>
-          This should be the title centered
+      <Row style={{ paddingBottom: 10, paddingTop: 10 }}>
+        <Col className={["d-flex justify-content-center w-100", styles.title]}>
+          Check and Add new cities!
         </Col>
       </Row>
       <Row>
-        <Col style={{ backgroundColor: "lightblue" }}>
-          <Button onClick={handleShowModal}>Add City</Button>
+        <Col className="d-flex justify-content-center w-100">
+          <Button variant="primary" onClick={handleShowModal}>
+            Add New City
+          </Button>
         </Col>
       </Row>
-      <Row
-        className="d-flex justify-content-start w-75 align-self-center"
-        style={{ backgroundColor: "green" }}
-      >
+      <Row className="d-flex justify-content-start w-75 align-self-center">
         {locations.map((location, index) => (
           <CityCard
             location={location}
             index={index}
             deleteCity={deleteCity}
             handleShowModal={handleShowModal}
+            handleShowCityModal={handleShowCityModal}
             setCurrentCity={setCurrentCity}
           />
         ))}
       </Row>
       <CustomModal
-        show={show}
+        showFormModal={showFormModal}
         handleClose={handleCloseModal}
         setUpdateList={setUpdateList}
+        currentCity={currentCity}
+      />
+      <CityModal
+        showCity={showCity}
+        handleClose={handleCloseCityModal}
         currentCity={currentCity}
       />
     </Container>
