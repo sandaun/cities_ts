98415327774c4f189dad423936cf79fa2892ec47
Oriol Carbo <oriol.carbo@gmail.com>
2021-06-20 22:25:38 +0200

    Update form by prefilling values if exist

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,17 @@ import { Form, Row, Col } from "react-bootstrap";
 
 const CustomForm = (props) => {
   const { handleSubmit, currentCity } = props;
-  console.log(99, currentCity);
+  console.log(currentCity);
   return (
     <Form onSubmit={handleSubmit} id="city-form">
       <Form.Group className="mb-3" controlId="name">
         <Form.Label>Name</Form.Label>
-        <Form.Control required type="text" placeholder="Enter City Name" />
+        <Form.Control
+          required
+          type="text"
+          placeholder="Enter City Name"
+          defaultValue={currentCity ? currentCity.title : null}
+        />
         <Form.Text style={{ color: "red" }}>*Required field</Form.Text>
       </Form.Group>
 
@@ -18,6 +23,7 @@ const CustomForm = (props) => {
           required
           type="text"
           placeholder="Enter City Description"
+          defaultValue={currentCity ? currentCity.content : null}
         />
         <Form.Text style={{ color: "red" }}>*Required field</Form.Text>
       </Form.Group>
@@ -25,18 +31,30 @@ const CustomForm = (props) => {
       <Row className="mb-3">
         <Form.Group as={Col} controlId="longitude">
           <Form.Label>Coordinates Longitude</Form.Label>
-          <Form.Control type="number" placeholder="Enter longitude" />
+          <Form.Control
+            type="number"
+            placeholder="Enter longitude"
+            defaultValue={currentCity ? currentCity.long : null}
+          />
         </Form.Group>
 
         <Form.Group as={Col} controlId="latitude">
           <Form.Label>Coordinates Longitude</Form.Label>
-          <Form.Control type="number" placeholder="Enter latitude" />
+          <Form.Control
+            type="number"
+            placeholder="Enter latitude"
+            defaultValue={currentCity ? currentCity.lat : null}
+          />
         </Form.Group>
       </Row>
 
       <Form.Group className="mb-3" controlId="image">
         <Form.Label>Image URL</Form.Label>
-        <Form.Control type="text" placeholder="Enter URL" />
+        <Form.Control
+          type="text"
+          placeholder="Enter URL"
+          defaultValue={currentCity ? currentCity.image_url : null}
+        />
       </Form.Group>
     </Form>
   );
